459ebbda111fa6652eded89c5a0f32edabdd7620
Kaique Ramon <kaique.ramon49@gmail.com>
2024-03-17 01:30:02 -0300

    kaique
    tutorial
    
    https://www.youtube.com/watch?v=2ajD1GDbEzA&t=1766s
    
    32:30 min

---
+++ Jogo/Assets/InventoryItem.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryItem : MonoBehaviour
+{
+    public int sizeWidth = 1;
+    public int sizeHeight = 1;
+
+
+}
