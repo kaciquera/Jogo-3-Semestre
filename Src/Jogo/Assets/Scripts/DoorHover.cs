d0ded32e77498071623f0290789c84e1cb00dabb
Akira <blurkiraa@gmail.com>
2024-11-22 19:24:49 -0300

    att

--- Src/Jogo/Assets/Scripts/DoorHover.cs
+++ Src/Jogo/Assets/Scripts/DoorHover.cs
@@ -3,80 +3,34 @@ using UnityEngine.EventSystems;
 
 namespace Game
 {
-    public class DoorHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    public class DoorHover : MonoBehaviour, IPointerEnterHandler
     {
+        [SerializeField] private GameObject buttons;
+
         private Animator _animator;
         private readonly int initiallizeHash = Animator.StringToHash("OnInitiallize");
         private readonly int activeHash = Animator.StringToHash("isActive");
 
-        [SerializeField] private GameObject buttons; // Arraste o contêiner dos botões aqui no Inspector.
-        private bool isMouseOverDoor = false;
-        private bool isMouseOverButtons = false;
-
         private void Awake()
         {
             _animator = GetComponent<Animator>();
-            if (buttons != null)
-            {
-                buttons.SetActive(false); 
-            }
+            buttons.SetActive(false);
         }
 
         public void OnPointerEnter(PointerEventData eventData)
         {
-            isMouseOverDoor = true;
             OpenDoor();
-        }
-
-        public void OnPointerExit(PointerEventData eventData)
-        {
-            isMouseOverDoor = false;
-            CheckAndCloseDoor();
         }
 
         private void OpenDoor()
         {
             _animator.SetTrigger(initiallizeHash);
             _animator.SetBool(activeHash, true);
-
-           
-            if (buttons != null)
-            {
-                float animationLength = _animator.GetCurrentAnimatorStateInfo(0).length;
-                Invoke(nameof(ShowButtons), animationLength); 
-            }
         }
 
-        private void ShowButtons()
+        public void ShowButtons()
         {
-            if (isMouseOverDoor) 
-            {
-                buttons.SetActive(true);
-            }
-        }
-
-        private void CheckAndCloseDoor()
-        {
-            
-            if (!isMouseOverDoor && !isMouseOverButtons)
-            {
-                _animator.SetBool(activeHash, false);
-                if (buttons != null)
-                {
-                    buttons.SetActive(false);
-                }
-            }
-        }
-
-        public void OnMouseEnterButtons()
-        {
-            isMouseOverButtons = true;
-        }
-
-        public void OnMouseExitButtons()
-        {
-            isMouseOverButtons = false;
-            CheckAndCloseDoor();
+            buttons.SetActive(true);
         }
     }
 }
